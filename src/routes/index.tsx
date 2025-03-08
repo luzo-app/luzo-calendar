226089bedf4b53ab771afa3425ecd71996172d94
Enzo QUELENIS <equelenis@myges.fr>
2025-03-08 12:52:14 +0100

    remove auth check

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,17 +5,9 @@ import {
     createBrowserRouter,
 } from "react-router";
 
-import { useAuth } from "@/hooks/use-auth";
-
 import Loader from "@/components/ui/loader";
 
 import { routes } from "@/data";
-
-// Protected route
-import { ProtectedRoute } from "./protected-route";
-
-// Helpers
-// import roleHelpers from "@/helpers/role";
 
 // Public Page
 const NotFoundPage = React.lazy(() => import('@/views/error/not-found'));
@@ -24,38 +16,18 @@ const NotFoundPage = React.lazy(() => import('@/views/error/not-found'));
 const HomePage = React.lazy(() => import('@/views/home'));
 
 const Routes = () => {
-    const { token } = useAuth();
-
     // Define public routes accessible to all users
     const routesForPublic = [
         {
             path: "*",
             element: <NotFoundPage />,
         },
-        // {
-        //     path: publicRoutes.LOGOUT,
-        //     element: <LogoutPage />,
-        // },
-    ];
-
-    // Define routes accessible only to non-authenticated users
-    const routesForNotAuthenticatedOnly = [
-        {
-            path: routes.HOME,
-            children: [
-                {
-                    path: routes.HOME,
-                    element: <NotFoundPage />,
-                },
-            ]
-        }
     ];
 
     // Define routes accessible only to guest users
     const routesForAuthenticatedOnly = [
         {
             path: routes.HOME,
-            element: <ProtectedRoute />,
             children: [
                 {
                     path: routes.HOME,
@@ -75,7 +47,6 @@ const Routes = () => {
     // Combine and conditionally include routes based on authentication status
     const router = createBrowserRouter([
         ...routesForPublic,
-        ...(!token ? routesForNotAuthenticatedOnly : []),
         ...routesForAuthenticatedOnly,
     ]);
 
@@ -84,3 +55,99 @@ const Routes = () => {
 };
 
 export default Routes;
+
+
+
+
+
+
+
+
+
+
+
+// ROUTES WITH AUTHENTICATION
+
+// import React, { Suspense } from "react";
+
+// import {
+//     RouterProvider,
+//     createBrowserRouter,
+// } from "react-router";
+
+// import { useAuth } from "@/hooks/use-auth";
+
+// import Loader from "@/components/ui/loader";
+
+// import { routes } from "@/data";
+
+// // Protected route
+// import { ProtectedRoute } from "./protected-route";
+
+// // Public Page
+// const NotFoundPage = React.lazy(() => import('@/views/error/not-found'));
+
+// // Guest Pages
+// const HomePage = React.lazy(() => import('@/views/home'));
+
+// const Routes = () => {
+//     const { token } = useAuth();
+
+//     // Define public routes accessible to all users
+//     const routesForPublic = [
+//         {
+//             path: "*",
+//             element: <NotFoundPage />,
+//         },
+//         // {
+//         //     path: publicRoutes.LOGOUT,
+//         //     element: <LogoutPage />,
+//         // },
+//     ];
+
+//     // Define routes accessible only to non-authenticated users
+//     const routesForNotAuthenticatedOnly = [
+//         {
+//             path: routes.HOME,
+//             children: [
+//                 {
+//                     path: routes.HOME,
+//                     element: <NotFoundPage />,
+//                 },
+//             ]
+//         }
+//     ];
+
+//     // Define routes accessible only to guest users
+//     const routesForAuthenticatedOnly = [
+//         {
+//             path: routes.HOME,
+//             element: <ProtectedRoute />,
+//             children: [
+//                 {
+//                     path: routes.HOME,
+//                     element:
+//                         <Suspense
+//                             fallback={
+//                                 <Loader className="absolute top-1/3 left-1/2" />
+//                             }
+//                         >
+//                             <HomePage />
+//                         </Suspense>,
+//                 },
+//             ]
+//         }
+//     ];
+
+//     // Combine and conditionally include routes based on authentication status
+//     const router = createBrowserRouter([
+//         ...routesForPublic,
+//         ...(!token ? routesForNotAuthenticatedOnly : []),
+//         ...routesForAuthenticatedOnly,
+//     ]);
+
+//     // Provide the router configuration using RouterProvider
+//     return <RouterProvider router={router} />;
+// };
+
+// export default Routes;
