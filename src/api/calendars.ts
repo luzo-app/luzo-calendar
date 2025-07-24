87687771200eea7823c54c188c28da98e2ff1ba5
Enzo QUELENIS <equelenis@myges.fr>
2025-07-25 01:34:44 +0200

    fix mixed content

--- src/api/calendars.ts
+++ src/api/calendars.ts
@@ -5,18 +5,18 @@ import { PaginatedResponse } from "@/types/api";
 
 const calendarService = {
   getCalendars: async (): Promise<PaginatedResponse<Calendar>> => {
-    const response = await api.get<PaginatedResponse<Calendar>>("/calendars")
+    const response = await api.get<PaginatedResponse<Calendar>>("/calendars/")
     return response.data
   },
   deleteCalendar: async (id: Calendar["_id"]): Promise<void> => {
-    await api.delete(`/calendars/${id}`)
+    await api.delete(`/calendars/${id}/`)
   },
   createCalendar: async (data: Omit<Calendar, "_id">): Promise<Calendar> => {
-    const response = await api.post<Calendar>("/calendars", data)
+    const response = await api.post<Calendar>("/calendars/", data)
     return response.data
   },
   updateCalendar: async (id: Calendar["_id"], data: Calendar): Promise<Calendar> => {
-    const response = await api.put<Calendar>(`/calendars/${id}`, data)
+    const response = await api.put<Calendar>(`/calendars/${id}/`, data)
     return response.data
   },
 };
